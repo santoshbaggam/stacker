adb0ac06432574a31f41f792552ab9cd45aa42c4
Santosh Baggam <santoshbaggam@gmail.com>
2017-09-19 18:36:16 +0530

    add installation file

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sudo curl -sSL https://raw.githubusercontent.com/santoshbaggam/stacker/master/stacker.sh > stacker
+
+sudo chmod +x ./stacker
+
+sudo mv ./stacker /usr/local/bin/
